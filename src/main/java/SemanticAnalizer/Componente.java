f32c8796494056f6eb752ae32c2b08472c3a1a49
oscar.azofeifa.segura@gmail.com <oscar.azofeifa.segura@gmail.com>
2018-05-27 01:05:17 -0600

    Nada

--- src/main/java/SemanticAnalizer/Componente.java
+++ src/main/java/SemanticAnalizer/Componente.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public interface Componente {
-
     public Componente getHermanoDerecho();
     public Componente getHijoMasIzq();
     public HashMap< String, Nombre > getTblSimbolosLocales();
