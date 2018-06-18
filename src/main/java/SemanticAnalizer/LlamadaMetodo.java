e2e841143f63ae999110967e142c52d851e02359
danivarelas <danivarelas@hotmail.com>
2018-06-17 23:16:44 -0600

    chequeo de bool commpleto y parametros casi completo

--- src/main/java/SemanticAnalizer/LlamadaMetodo.java
+++ src/main/java/SemanticAnalizer/LlamadaMetodo.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class LlamadaMetodo extends Sentencia implements Expresion, Nombre {
 
-    private List<Variable> _parametros;
+    private List<ExpresionGenerico> _parametros;
     private String _nombre;
     private Tipo _tipo;
 
@@ -45,5 +45,13 @@ public class LlamadaMetodo extends Sentencia implements Expresion, Nombre {
     @Override
     public Tipo get_tipo() {
         return _tipo;
+    }
+
+    public List<ExpresionGenerico> get_parametros() {
+        return _parametros;
+    }
+
+    public void set_parametros(List<ExpresionGenerico> _parametros) {
+        this._parametros = _parametros;
     }
 }
