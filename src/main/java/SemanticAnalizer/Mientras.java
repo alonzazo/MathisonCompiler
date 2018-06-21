2d363b54fa777862578ca12bc55b0b307e84a107
Karman <krmanxv@gmail.com>
2018-06-21 01:01:47 -0600

    Primera parte del Mientras

--- src/main/java/SemanticAnalizer/Mientras.java
+++ src/main/java/SemanticAnalizer/Mientras.java
@@ -6,12 +6,24 @@ import java.util.List;
 
 public class Mientras extends Estructura {
 
+    private Componente _condicion;
+
     public Mientras(){
 
     }
 
     public Mientras(Componente sentencias){
         _hijoMasIzq = sentencias;
+    }
+
+    public Componente get_condicion()
+    {
+        return _condicion;
+    }
+
+    public void set_condicion(Componente condicion)
+    {
+        _condicion = condicion;
     }
 
     @Override
