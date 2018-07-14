7b70c6acbdb1f77fec4d09e7ea5d1e527000ecd5
Oscar Azofeifa <oscar.azofeifa.segura@gmail.com>
2018-07-14 14:51:38 -0600

    compilar() en imprimir casi hecho

--- src/main/java/SemanticAnalizer/Imprimir.java
+++ src/main/java/SemanticAnalizer/Imprimir.java
@@ -24,7 +24,21 @@ public class Imprimir extends Sentencia {
 
     //@Override
     public String compilar() throws SemanticError {
+        String result = "";
         //TODO Compilar del imprimir
-        return "";
+        Tipo tipoExpresion = _expresion.evaluarTipo();
+        if (tipoExpresion == Tipo.NUMERICO){
+            result = "\tlw \t\t$a0, " + _expresion.compilar() + "\n" +
+                     "\tli \t\t$v0, 1\t\t##println(int)\n" +
+                     "\tsyscall";
+        } else if (tipoExpresion == Tipo.CADENA){
+            result = "\tli $v0, 4 \t\t\t\t# Carga system call code para el print\n" +
+                     "\tla $a0, " + _expresion.compilar() + " \t\t# Carga la direccion a a0\n" +
+                     "\tsyscall ";
+        } else {
+            //TODO agregar todo la evaluación lógica de la expresion
+            result = _expresion.compilar();
+        }
+        return result;
     }
 }
